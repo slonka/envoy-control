e62c2dbc76cb887f32df231cf762e9a7be994d32
slonka <slonka@users.noreply.github.com>
2020-06-21 14:37:41 +0200

    Speed up curl by using retries, try to wait for 8080 port EC

--- heroku/run-envoy.sh
+++ heroku/run-envoy.sh
@@ -15,11 +15,13 @@ echo "Launching Envoy-control with $START_ARGUMENTS"
 
 # start envoys
 sed -i "s/{{.IngressListenerPort}}/${PORT:-10000}/g" /etc/envoy-front-proxy.yaml
-sh -c 'sleep 20; /usr/local/bin/envoy --base-id 1 -c /etc/envoy1.yaml' &
-sh -c 'sleep 20; /usr/local/bin/envoy --base-id 2 -c /etc/envoy2.yaml' &
 /usr/local/bin/envoy -c /etc/envoy-front-proxy.yaml &
 
-sh -c 'sleep 25; curl -X PUT -s localhost:8500/v1/agent/service/register -T /etc/envoy-control/register-echo1.json' &
-sh -c 'sleep 25; curl -X PUT -s localhost:8500/v1/agent/service/register -T /etc/envoy-control/register-echo2.json' &
+sh -c 'curl --max-time 1 --retry 25 -X PUT -s localhost:8500/v1/agent/service/register -T /etc/envoy-control/register-echo1.json' &
+sh -c 'curl --max-time 1 --retry 25 -X PUT -s localhost:8500/v1/agent/service/register -T /etc/envoy-control/register-echo2.json' &
+
+sh -c 'while ! nc -z localhost 8080; do
+  sleep 1 #
+done && /usr/local/bin/envoy --base-id 1 -c /etc/envoy1.yaml &; /usr/local/bin/envoy --base-id 2 -c /etc/envoy2.yaml &' &
 
 consul agent -server -ui -ui-content-path "/consul/ui" -dev -client 0.0.0.0
